c47eb32f5f8b25b1d3a333777fb4d204a599fe8d
franciscofs2003 <99775859+franciscofs2003@users.noreply.github.com>
2024-05-21 10:52:06 -0600

    Initial commit

---
+++ actividad6d.js
@@ -0,0 +1,11 @@
+function toggleContrast() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function increaseTextSize() {
+    document.body.classList.add('large-text');
+}
+
+function decreaseTextSize() {
+    document.body.classList.remove('large-text');
+}
